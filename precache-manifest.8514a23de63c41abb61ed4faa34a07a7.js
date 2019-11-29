284bad6be604aaa4adb0281b3452aaaadda56d01
ucheka22 <ryanucheka@gmail.com>
2019-11-29 11:39:41 +0100

    Updates

---
+++ precache-manifest.8514a23de63c41abb61ed4faa34a07a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac8b7562d6746ada1a6f44438d7aa75",
+    "url": "/teamwork_UI/index.html"
+  },
+  {
+    "revision": "6732d7e1ecc7ec874f27",
+    "url": "/teamwork_UI/static/css/main.157ed38d.chunk.css"
+  },
+  {
+    "revision": "6f4dba51f71962c1467f",
+    "url": "/teamwork_UI/static/js/2.9b3bf462.chunk.js"
+  },
+  {
+    "revision": "6732d7e1ecc7ec874f27",
+    "url": "/teamwork_UI/static/js/main.6a4bb149.chunk.js"
+  },
+  {
+    "revision": "f9f64176a65db12051e4",
+    "url": "/teamwork_UI/static/js/runtime-main.43ed25bf.js"
+  },
+  {
+    "revision": "c55b2c3bcd8af24a6033230af8b5e6ad",
+    "url": "/teamwork_UI/static/media/MrBean.c55b2c3b.gif"
+  },
+  {
+    "revision": "892b3f33789a99517849e9373b4761b6",
+    "url": "/teamwork_UI/static/media/facebook.892b3f33.png"
+  },
+  {
+    "revision": "9a513524f2be4401637c4fee6191cd2e",
+    "url": "/teamwork_UI/static/media/twitter.9a513524.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/teamwork_UI/precache-manifest.8514a23de63c41abb61ed4faa34a07a7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teamwork_UI/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
